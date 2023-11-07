cc74ec4bac05617259e314400ae50f2d78d72310
Georgy <cial.dev.inc@gmail.com>
2023-11-07 15:14:24 +0100

    2

--- tests/demo-todo-app.spec.ts
+++ tests/demo-todo-app.spec.ts
@@ -3,11 +3,11 @@ import { test, expect, type Page } from '@playwright/test';
 test.beforeEach(async ({ page }) => {
   const inputLogin = page.locator("#session_key");
   const inputPass = page.locator("#session_password");
-  const signInButton = page.getByText("Sign in")
+  const signInButton = page.getByText("Sign in").nth(2)
   await page.goto('https://www.linkedin.com');
   await inputLogin.fill(user)
   await inputPass.fill(password)
-  await signInButton.click
+  await signInButton.click()
 });
 
 const TITLES = ['QA',
