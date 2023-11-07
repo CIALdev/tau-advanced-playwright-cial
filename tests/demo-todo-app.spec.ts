a9939abfbb9ca282deb5467156d9148fba9b2216
Georgy <cial.dev.inc@gmail.com>
2023-11-07 16:21:36 +0100

    8

--- tests/demo-todo-app.spec.ts
+++ tests/demo-todo-app.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect, type Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  const inputLogin = page.locator("#session_key");
-  const inputPass = page.locator("#session_password");
-  const signInButton = page.getByText("Sign in").nth(1)
-  await page.goto('https://www.linkedin.com');
-  await signInButton.click()
-  await inputLogin.fill(user)
-  await inputPass.fill(password)
-  await signInButton.click()
+  // const inputLogin = page.locator("#session_key");
+  // const inputPass = page.locator("#session_password");
+  // const signInButton = page.getByText("Sign in").nth(1)
+  await page.goto('https://www.linkedin.com/jobs/search');
+  // await signInButton.click()
+  // await inputLogin.fill(user)
+  // await inputPass.fill(password)
+  // await signInButton.click()
 });
 
 const TITLES = ['QA',
@@ -23,13 +23,14 @@ const password = 'FindMePlaywrightJobs';
 test.describe('Find Playwright jobs', () => {
   test('should allow me to find jobs', async ({ page }) => {
     // create a new todo locator
-    const jobTitle = page.getByPlaceholder("Search by title, skill, or company");
-    const jobLocation = page.getByPlaceholder("City, state, or zip code");
+    const jobTitle = page.getByLabel("Search job titles or companies");
+    const jobLocation = page.getByLabel('Location')
+    const buttonSearch = page.getByLabel('Search')
 
     // Fill
     await jobTitle.fill(TITLES[0]);
     await jobLocation.fill('EMEA');
-    await jobLocation.press('Enter');
+    await buttonSearch.click();
 
     // Make sure the list now has two todo items.
     await expect(page.getByRole("alert")).not.toBeVisible()
