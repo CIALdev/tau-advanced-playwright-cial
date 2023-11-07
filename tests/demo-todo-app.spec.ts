d55c13d4807b91c099b5749bf6ac9bdd8385bca9
Georgy <cial.dev.inc@gmail.com>
2023-11-07 16:35:57 +0100

    kj

--- tests/demo-todo-app.spec.ts
+++ tests/demo-todo-app.spec.ts
@@ -1,14 +1,13 @@
 import { test, expect, type Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  // const inputLogin = page.locator("#session_key");
-  // const inputPass = page.locator("#session_password");
-  // const signInButton = page.getByText("Sign in").nth(1)
-  await page.goto('https://www.linkedin.com/jobs/search');
-  // await signInButton.click()
-  // await inputLogin.fill(user)
-  // await inputPass.fill(password)
-  // await signInButton.click()
+  const inputLogin = page.getByLabel("Email or Phone");
+  const inputPass = page.getByLabel("Password");
+  const signInButton = page.getByLabel("Sign in",{exact: true})
+  await page.goto('https://www.linkedin.com/checkpoint/lg/sign-in-another-account');
+  await inputLogin.fill(user)
+  await inputPass.fill(password)
+  await signInButton.click()
 });
 
 const TITLES = ['QA',
