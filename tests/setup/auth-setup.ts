9b5d6abdeda9677e22e2ce88c8fcf42ed49cbb3e
Georgy <cial.dev.inc@gmail.com>
2023-11-07 14:38:14 +0100

    new

--- tests/setup/auth-setup.ts
+++ tests/setup/auth-setup.ts
@@ -2,16 +2,6 @@ import { test as setup, type Page } from '@playwright/test';
 import LoginPage from '../ui/pages/login-page';
 import uiPages from '../utils/uiPages';
 
-const adminFile = '.auth/admin.json';
-
-setup('authenticate as admin', async ({ page }) => {
-  const user = process.env.USERNAME_ADMIN!;
-  const password = process.env.PASSWORD!;
-  await doLogin(page, user, password);
-
-  await page.context().storageState({ path: adminFile });
-});
-
 const userFile = '.auth/user.json';
 
 setup('authenticate as user', async ({ page }) => {
